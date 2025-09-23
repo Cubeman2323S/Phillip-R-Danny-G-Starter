b7fa571823af05de6999446caf62552e04f6df09
Cubeman2323S <probbins2238@student.pps.net>
2025-09-23 09:00:49 -0700

    Fixed coins

--- scripts/coins.gd
+++ scripts/coins.gd
@@ -2,18 +2,7 @@ extends Node
 
 
 func _on_body_entered(body):
-<<<<<<< Updated upstream
 	if body.name == "CharacterBody2D":
-=======
-<<<<<<< HEAD
-	if body.name == "Player":
-		body.change_coins(1)
-		queue_free()
-=======
-	if body.name == "CharacterBody2D":
->>>>>>> 4524b769df2c59c348d99515c4841a447eb50085
->>>>>>> Stashed changes
-	# TODO: Check if the object that touched the coin is the player
 		body.change_coins(1)
 		queue_free()
 	
