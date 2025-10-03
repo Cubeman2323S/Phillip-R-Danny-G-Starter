aabda9f91df0b462f9e56034cdb27c7f610daaec
Cubeman2323S <probbins2238@student.pps.net>
2025-10-03 09:24:16 -0700

    Skitzoh nighmares (projectiles)

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -2,6 +2,8 @@ extends Area2D
 var speed = 200
 var direction
 var target
+var timerstart = 3
+var timer = timerstart
 func set_direction(target):
 	direction = position.direction_to(target)
 	pass
@@ -14,3 +16,7 @@ func _on_body_entered(body):
 		body.change_health(-2)
 		queue_free()
 	pass
+func _process(delta: float):
+	timer -= delta
+	if timer < 0:
+		queue_free()
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,4 +1,8 @@
 extends CharacterBody2D
+var timerstart = 2
+var timer = timerstart
+var radius = false
+var player
 
 var projectile_original = preload("res://scenes/enemy_projectile.tscn")
 
@@ -7,27 +11,28 @@ func _ready():
 	pass
 
 func _process(delta: float) -> void:
+	if radius == true:
+		timer -= delta
+		if timer < 0:
+			shoot(player)
+			timer = timerstart
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	player = body
 	if body.name == "Player":
-		
-		# TODO: Create a new projectile instance
-		var projectile_clone = projectile_original.instantiate()
-	
-	# TODO: Set projectile position to player position
-		projectile_clone.global_position = position
-	
-	# TODO: Set projectile direction using facing variable
-		projectile_clone.set_direction(body.position)
-	
-	# TODO: Add projectile to the game world
-		get_tree().get_root().add_child(projectile_clone)
+		radius = true
 
 	pass
-
+func shoot(body):
+	if body.name == "Player":
+		var projectile_clone = projectile_original.instantiate()
+		projectile_clone.global_position = position
+		projectile_clone.set_direction(body.position)
+		get_tree().get_root().add_child(projectile_clone)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	
+	if body.name == "Player":
+		radius = false
 	
 	pass
